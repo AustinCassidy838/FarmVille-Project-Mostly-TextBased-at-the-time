a67547afb8f129ed275c83cbd2d00716f7fc6f2d
AustinCassidy838 <austcalebd@yahoo.com>
2024-12-07 16:15:25 -0800

    Add files via upload

---
+++ scripts.js.js
@@ -0,0 +1,8 @@
+let coins = 600;
+function addCoins() {}
+coins += 10;
+console.log('Total Coins: ' + coins);
+{}
+<button id="Add 10 Coins">Add 10 coins</button>
+console.log('you now have ${coins} coins.');
+function addCoins() {}
